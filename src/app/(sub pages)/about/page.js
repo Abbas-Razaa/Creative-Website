c8bec0b4285009134531837c0cab9ea2b81d2327
Abbas Raza <abbasrazaa02@gmail.com>
2024-09-27 23:32:45 +0500

    Added projects

--- src/app/(sub pages)/about/page.js
+++ src/app/(sub pages)/about/page.js
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import bg from "../../../../public/background/about-background.png";
 import RenderModel from "@/components/RenderModel";
-// import HatModel from "@/components/models/HatModel";
 import AboutDetails from "@/components/about";
 import dynamic from "next/dynamic";
 const HatModel = dynamic(() => import("@/components/models/HatModel"), {
@@ -32,7 +31,7 @@ export default function Home() {
       <div className="relative w-full h-screen flex flex-col items-center justify-center">
         <div className="absolute flex flex-col items-center text-center top-1/2 sm:top-[60%] left-1/2 -translate-y-1/2 -translate-x-1/2">
           <h1 className="font-bold  text-5xl xs:text-6xl sm:text-7xl  lg:text-8xl text-accent">
-            Code Wizard
+            Abbas Raza
           </h1>
           <p className="font-light text-foreground text-lg">
             Meet the wizard behind this portfolio
